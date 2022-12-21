f036a7e6e90940857962893d1a56bffb98bddfb9
RICARDO FREITAS <rmotafreitas@gmail.com>
2022-12-21 16:49:25 +0000

    Reading roles in a json file

--- bot.py
+++ bot.py
@@ -3,70 +3,50 @@ from dotenv import load_dotenv
 import os
 import discord.ui
 from discord.ext import commands
+import json
 
 intents = discord.Intents.all()
-
 bot = commands.Bot(command_prefix='.', intents=intents, help_command = None)
+
+# Setup Roles Array Information for developer roles buttons
+dictDataRoles = json.load(open('button.roles.json'))
+arrRolesDev = dictDataRoles['devRoles']
 
 @bot.event
 async def on_ready():
     print('System Ready')
-    bot.add_view(Roles())
 
-class Roles(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
+async def handleBtnRoleClick(interaction, role):
+    user = interaction.user
+    if role in [y.id for y in user.roles]:
+        await user.remove_roles(user.guild.get_role(role))
+        await interaction.response.send_message('Você removeu a Role!', ephemeral = True, delete_after = 5.0)
+    else:
+        await user.add_roles(user.guild.get_role(role))
+        await interaction.response.send_message('Você adicionou a Role!', ephemeral = True, delete_after = 5.0)
 
-    async def apply(self, interaction, role):
-        if role in [y.id for y in interaction.user.roles]:
-            await interaction.user.remove_roles(interaction.user.guild.get_role(role))
-            await interaction.response.send_message('Você removeu a Role!', ephemeral = True, delete_after = 5.0)
-        else:
-            await interaction.user.add_roles(interaction.user.guild.get_role(role))
-            await interaction.response.send_message('Você adicionou a Role!', ephemeral = True, delete_after = 5.0)
+class RoleBtn(discord.ui.Button):
+    def __init__(self, label='0', **kwargs):
+        super().__init__(label=label, **kwargs)
 
-    #Back-end
-    @discord.ui.button(label = "👩‍💻Back-end", custom_id = "Back-end", style = discord.ButtonStyle.secondary)
-    async def button1(self, interaction, button):
-        await self.apply(interaction, 992133047954702477)
-    #Front-end
-    @discord.ui.button(label = "🧙‍♂️Front-end", custom_id = "Front-end", style = discord.ButtonStyle.secondary)
-    async def button2(self, interaction, button):
-        await self.apply(interaction, 992133005000843336)
-    #Data
-    @discord.ui.button(label = "📈Data Science", custom_id = "Data Science", style = discord.ButtonStyle.secondary)
-    async def button3(self, interaction, button):
-        await self.apply(interaction, 992133977882230825)
-    #Python
-    @discord.ui.button(emoji = '<:des_python62:1053438694767480903>' , label = "Python", custom_id = "Python", style = discord.ButtonStyle.secondary)
-    async def button4(self, interaction, button):
-        await self.apply(interaction, 1053439212814348348)
-    #C/C++
-    @discord.ui.button(emoji = '<:cpp:1054473082225573948>' , label = "C/C++", custom_id = "C/C++", style = discord.ButtonStyle.secondary)
-    async def button9(self, interaction, button):
-        await self.apply(interaction, 1054472343839322122)
-    #Java/JavaScript
-    @discord.ui.button(emoji = '<:java11:1053438696571015278>' , label = "Java/JavaScript", custom_id = "Java/JavaScript", style = discord.ButtonStyle.secondary)
-    async def button5(self, interaction, button):
-        await self.apply(interaction, 1053439219713982535)
-    #TypeScript
-    @discord.ui.button(emoji = '<:ts:1053648211719503914>' , label = "TypeScript", custom_id = "TypeScript", style = discord.ButtonStyle.secondary)
-    async def button6(self, interaction, button):
-        await self.apply(interaction, 1053719075601252503)
-    #CSharp
-    @discord.ui.button(emoji = '<:csharp:1054049040641298472>' , label = "C#", custom_id = "C#", style = discord.ButtonStyle.secondary)
-    async def button7(self, interaction, button):
-        await self.apply(interaction, 1054428575220113468)
-    #Html/Css
-    @discord.ui.button(emoji = '<:css:1054471552453849120>' , label = "Html/Css", custom_id = "Html/Css", style = discord.ButtonStyle.secondary)
-    async def button8(self, interaction, button):
-        await self.apply(interaction, 1054471923339370566)
-
-
+    async def callback(self, interaction: discord.Interaction):
+        for role in arrRolesDev:
+                if role['id_custom'] == self.custom_id:
+                    await handleBtnRoleClick(interaction, role['id_role'])
+                    
 @bot.command()
 async def roles(ctx):
     embed = discord.Embed(title = "Selecionar Roles", description='Reaja com a sua role correspondente:')
-    await ctx.send(embed = embed, view = Roles())
+    view = discord.ui.View(timeout=None)
+    
+    # Create a button for each role
+    for role in arrRolesDev:
+        if "emoji" in role:
+            view.add_item(RoleBtn(emoji=role['emoji'], label=role['label'], custom_id=role['id_custom'], style=discord.ButtonStyle.secondary))
+        else:
+            view.add_item(RoleBtn(label=role['label'], custom_id=role['id_custom'], style=discord.ButtonStyle.secondary))
+
+    await ctx.send(embed = embed, view = view)
 
 intents = discord.Intents.default()
 intents.message_content = True
