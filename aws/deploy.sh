e094d6464ca6db585a7824697b19bdb0194665cf
Carlos Dias <carlos.dias@dreamshaper.com>
2022-12-22 11:22:25 +0000

    docker compose template location

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -29,12 +29,12 @@ io_info "retrieving code version ...\n"
 tag="$( git describe --tags )"
 io_success "code version found : $tag\n"
 
-io_info "checking if there is a docker image for the founded tag ...\n"
-if [ "$( sudo docker images | grep botlivesdoctor | grep $tag )" ]
-then
-    io_success "docker image for current version already founded\n"
-    exit 0
-fi
+# io_info "checking if there is a docker image for the founded tag ...\n"
+# if [ "$( sudo docker images | grep botlivesdoctor | grep $tag )" ]
+# then
+#     io_success "docker image for current version already founded\n"
+#     exit 0
+# fi
 
 io_info "building docker image ...\n"
 sudo docker build -t botlivesdoctor:$tag .
@@ -45,7 +45,7 @@ io_success "docker image builded\n"
 ################################################################################
 
 io_info "updating the docker compose yaml ...\n"
-sed "s|<tag>|$tag|g" < "$(path $dir/../docker-compose.template)" > "$(path $dir/../docker-compose.yaml)"
+sed "s|<tag>|$tag|g" < "$(path $dir/docker-compose.template)" > "$(path $dir/../docker-compose.yaml)"
 io_success "docker compose yaml updated\n"
 
 ################################################################################
